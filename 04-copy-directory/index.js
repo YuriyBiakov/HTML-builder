a4d93052e437d8a1fc438effbc0737eceace9de6
Yuriy Biakov <gbyackov@gmail.com>
2025-01-20 13:21:34 +0300

    develop: update constants

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const copiedFolderPath = './04-copy-directory/files';
+const targetFolderpath = './04-copy-directory/files-copy';
 
-async function copyDir() {
+async function copyDir(scr, targetSrc) {
 
 }
