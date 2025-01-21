d0956e726ba6d128285bcb2783aff57aa838b3f2
Yuriy Biakov <gbyackov@gmail.com>
2025-01-21 14:52:17 +0300

    fix: fix task-05 - add update functionality when rerun script

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,33 +3,35 @@ const path = require('path');
 
 const bundleSrc = './05-merge-styles/project-dist/bundle.css';
 const stylesSrc = './05-merge-styles/styles';
-mergeStyles(stylesSrc, bundleSrc);
 
 
-fs.rm(bundleSrc, {recursive: true}, () => {
-    async function mergeStyles(stylesSrc, bundleSrc) {
-        fs.readdir(stylesSrc, {withFileTypes: true}, (err, files) => {
-            if (err) console.log('fs.reddir ERROR! ', err);
-            files.forEach(file => {
-                if (!file.isDirectory()) {
-                    const filePath = path.join(stylesSrc, file.name);
-                    const fileExt = path.extname(file.name);
-                    if (fileExt === '.css') {
-                        fs.readFile(filePath, 'utf8', (err, data) => {
-                            if (err) {
-                              console.log('fs.readFile Error!', err);
-                              return;
-                            }
-                            fs.appendFile(bundleSrc, data, (err) => {
-                                  if (err) throw console.log('fsappendFile Error!', err);
-                                }
-                              );
-                          });
-                    }
-                }
-            });
-        })
-    }
+const deleteFunc = fs.rm(bundleSrc, {recursive: true}, () => {
+    mergeStyles(stylesSrc, bundleSrc);
 });
+
+
+async function mergeStyles(stylesSrc, bundleSrc) {
+    fs.readdir(stylesSrc, {withFileTypes: true}, (err, files) => {
+        if (err) console.log('fs.reddir ERROR! ', err);
+        files.forEach(file => {
+            if (!file.isDirectory()) {
+                const filePath = path.join(stylesSrc, file.name);
+                const fileExt = path.extname(file.name);
+                if (fileExt === '.css') {
+                    fs.readFile(filePath, 'utf8', (err, data) => {
+                        if (err) {
+                          console.log('fs.readFile Error!', err);
+                          return;
+                        }
+                        fs.appendFile(bundleSrc, data, (err) => {
+                              if (err) throw console.log('fsappendFile Error!', err);
+                            }
+                          );
+                      });
+                }
+            }
+        });
+    })
+}
 
 //TODO split the code into several functions when I understand asynchrony
